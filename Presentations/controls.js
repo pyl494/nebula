93536d1b2c258f15364f672bd64c63c8c4f299b1
H D <hd125@uowmail.edu.au>
2020-05-12 02:54:51 +1000

    progress on the presentation, with a UI concept in progress.

---
+++ Presentations/controls.js
@@ -0,0 +1,47 @@
+let timelineSizeShift = -(timelineSize * 1.0) / 2;
+let cooldown = Date.now();
+
+function zoomin(element){
+    if (Date.now() - cooldown > 1100){
+        let pRect = element.parentNode.getBoundingClientRect();
+        pRect.x = (pRect.right + pRect.left) / 2.0;
+        pRect.y = (pRect.bottom + pRect.top) / 2.0;
+
+        let rect = element.getBoundingClientRect();
+        rect.x = (rect.right + rect.left) / 2.0;
+        rect.y = (rect.bottom + rect.top) / 2.0;
+
+        element.className += ' timeline-node-active';
+        
+        element.parentNode.style.transform = `
+            scale(${1})
+            translate(
+                ${document.body.clientWidth / 2 - rect.x * scale }px, 
+                ${timelineSizeShift }px)
+            `;
+        element.parentNode.style.transformOrigin = `${0}px ${0}px`;/*`
+                ${rect.x / (pRect.right - pRect.left)}% 
+                ${rect.y / (pRect.bottom - pRect.top)}%`;*/
+                element.parentNode.style.transition = 'transform 1s ease-in-out, transform-origin 1s linear';
+        element.setAttribute('onclick', 'return zoomout(this);');
+
+        cooldown = Date.now();
+    }
+    return false;
+}
+
+function zoomout(element){
+    if (Date.now() - cooldown > 1100){
+        let rect = element.getBoundingClientRect();
+
+        element.className = element.className.replace(/\btimeline-node-active\b/g, "");
+
+        element.parentNode.style.transform = `translate(0px, 0px) scale(${1/scale})`;
+        element.parentNode.style.transformOrigin = `0px 0px`;
+        element.parentNode.style.transition = 'transform 1s ease-in-out';
+        element.setAttribute('onclick', 'return zoomin(this);');
+
+        cooldown = Date.now();
+    }
+    return false;
+}
