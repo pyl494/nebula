18c6ed4e47285f06aaf822cafb037986e0d63546
H D <hd125@uowmail.edu.au>
2020-05-16 12:56:04 +1000

    Removed the versionedRepresentation from dump_jira 2.js

--- Data Extraction/dump_jira 2.js
+++ Data Extraction/dump_jira 2.js
@@ -33,7 +33,7 @@ function getpage(data, pagesRemaining, page){
         
         console.log('GET', target.project, page);
         let query = encodeURIComponent(`ORDER BY priority DESC, updated DESC`);
-        let req = new Request(`${target.baseurl}rest/api/2/search?jql=${query}&validateQuery=false&maxResults=1000&startAt=${page}&fields=*all&expand=names,versionedRepresentations,changelog`);
+        let req = new Request(`${target.baseurl}rest/api/2/search?jql=${query}&validateQuery=false&maxResults=1000&startAt=${page}&fields=*all&expand=names,changelog`);
         fetch(req)
             .then(response => {
                 console.log(response.status, response.statusText);
