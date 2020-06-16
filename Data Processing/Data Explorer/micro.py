68b36b530d62e646477179d0df7bcbad8f304adc
H D <hd125@uowmail.edu.au>
2020-06-16 12:09:53 +1000

    - Include ability to manually override the ML to data explore /micro

--- Data Processing/Data Explorer/micro.py
+++ Data Processing/Data Explorer/micro.py
@@ -25,7 +25,7 @@ try:
                         issues = version_issue_map[version_name]
                         features = change_request.getExtractedFeatures(project_key, version_name, issues)
 
-                        if mlabel is None or mlabel == 'None':
+                        if mlabel is None or mlabel in ['low', 'medium', 'high']:
                             data = [{
                                     'number_of_issues': features['number_of_issues'],
                                     'number_of_bugs': features['number_of_bugs'],
@@ -50,13 +50,41 @@ try:
                                 prediction = clf.predict(X)[0]
                                 print(prediction)
                                 response['predictions'][name] = ['low', 'medium', 'high'][prediction]
-
+                        else:
+                            response['override'] = mlabel
                         
                         response['result'] = 'ok'
 
                         found = True
                         break
 
+        if not found:
+            response['result'] = 'Not Found'
+
+    elif querystring['type'] == 'override':
+        project_key = querystring['project']
+        version_name = querystring['version']
+        label = querystring['label']
+
+        found = False
+        for change_request in change_request_list:
+            issue_map = change_request.getIssueMap()
+            projects_fixVersion_issue_map = change_request.getProjectsFixVersionIssueMap()
+
+            if issue_map.getUniverseName() == 'Microservice Demo':
+                if project_key in projects_fixVersion_issue_map:
+                    version_issue_map = projects_fixVersion_issue_map[project_key]
+                    if version_name in version_issue_map:
+                        found = True
+                        change_request.setManualRiskLabel(project_key, version_name, label)
+                        mlabel = change_request.getManualRiskLabel(project_key, version_name)
+
+                        if label == mlabel:
+                            response['result'] = 'ok'
+                        else:
+                            response['result'] = 'Failed'
+
+                        break
         if not found:
             response['result'] = 'Not Found'
 
