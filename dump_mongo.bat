1217bdd7468db64300b9805c9db3788cac764406
H D <hd125@uowmail.edu.au>
2020-08-29 13:23:06 +1000

    The files necessary for dumping and restoring mongo data.

---
+++ dump_mongo.bat
@@ -0,0 +1,2 @@
+"./Third Party/mongodb-win32-x86_64-windows-4.4.0/bin/mongodump" --db="data-explorer" --gzip --out="F:/mongodumps"
+
---
+++ restore_mongo.bat
@@ -0,0 +1,2 @@
+"./Third Party/mongodb-win32-x86_64-windows-4.4.0/bin/mongorestore" "F:/mongodumps" --gzip
+
